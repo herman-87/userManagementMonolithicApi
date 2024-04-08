55c765e7f5becd4afa7906e80ba9eac1f70a5ac0
essoungou.wonga <essoungou.wonga@lao-sarl.cm>
2024-04-08 12:42:58 +0100

    first commit

---
+++ src/main/java/com/h87/userManagementMonolithicApi/UserManagementMonolithicApiApplication.java
@@ -0,0 +1,13 @@
+package com.h87.userManagementMonolithicApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementMonolithicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementMonolithicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/h87/userManagementMonolithicApi/UserManagementMonolithicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h87.userManagementMonolithicApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementMonolithicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
